3a034adab43d37cfb3fe04c77ab70ee6dbeb8513
Sandro Maisuradze <43883662+Sandrikam@users.noreply.github.com>
2021-09-25 00:16:43 -0700

    Update WebElementsTest.java

--- webTests/src/test/java/WebElementsTest.java
+++ webTests/src/test/java/WebElementsTest.java
@@ -1,8 +1,8 @@
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class WebElementsTest {
