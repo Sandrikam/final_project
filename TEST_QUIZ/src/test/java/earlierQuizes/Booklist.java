7452d8ef3d4b95e64960abaaec1a5a0046dcce55
sandrikam <sandromaisuradze32@gmail.com>
2022-01-04 00:33:49 +0400

    Repository Cleanup

--- TEST_QUIZ/src/test/java/earlierQuizes/Booklist.java
+++
@@ -1,8 +0,0 @@
-package Deserialize_new;
-
-import java.util.ArrayList;
-import java.util.List;
-
-class BookList {
-    ArrayList<Deserialize_new.Book> books;
-}
--- TEST_QUIZ/src/test/java/earlierQuizes/Request.java
+++
@@ -1,7 +0,0 @@
-package Deserialize_new;
-
-public class Request {
-    static String ISBN = "9781449325862";
-
-}
-
